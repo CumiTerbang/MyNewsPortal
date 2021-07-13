f00aa74a41837cdde76c7726f4b08126aa2d7afc
CumiTerbang <gitarringsek@gmail.com>
2021-07-13 09:51:18 +0700

    rework endless scroll recyclerview

--- app/src/main/java/com/haryop/mynewsportal/ui/searchpage/SearchFragment.kt
+++ app/src/main/java/com/haryop/mynewsportal/ui/searchpage/SearchFragment.kt
@@ -44,21 +44,19 @@ class SearchFragment : BaseFragmentBinding<FragmentNewslistPageBinding>(),
             val hashMap: HashMap<String, String> = HashMap<String, String>()
             hashMap.put("query", it)
             hashMap.put("page", "1")
+            is_added = false
             viewModel.start(hashMap)
             Log.e("onGetSourcesObserver", "onGetSourcesObserver(1)")
             onGetSourcesObserver(1)
         }
     }
 
-    private var loading = true
-    var pastVisiblesItems = 0
-    var visibleItemCount: Int = 0
-    var totalItemCount: Int = 0
+    private var isLoading = true
     var total_articles: Int = 0
     var page_param = 1
     var totalpage = 0
     private lateinit var adapter: NewsListAdapter
-    private lateinit var scrollListener: EndlessRecyclerViewScrollListener
+    private lateinit var scrollListener: RecyclerView.OnScrollListener
     fun setUpRecyclerView() = with(viewbinding) {
         adapter = NewsListAdapter(this@SearchFragment)
         adapter.setCurrent_page(adapter.SEARCH_PAGE)
@@ -75,33 +73,65 @@ class SearchFragment : BaseFragmentBinding<FragmentNewslistPageBinding>(),
                 // Add whatever code is needed to append new items to the bottom of the list
 //                comingSoon("\nDO LOAD MORE" +
 //                        "\npage=${page}")
-
-                var page_toload = page + 1
-
-                Log.e("load more", "page: ${page}")
-                Log.e("load more", "page toload: ${page_toload}")
-                Log.e("load more", "total page: ${totalpage}")
-
-                if (page_toload <= totalpage) {
-                    getData(query, page_toload)
+                if (isLoading == false) {
+                    doLoadMore()
                 }
 
             }
         }
 
+//        scrollListener = object : RecyclerView.OnScrollListener() {
+//            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+//                super.onScrolled(recyclerView, dx, dy)
+//
+//                if (dy > 0) {
+//                    val visibleItemCount: Int = linearLayoutManager.getChildCount()
+//                    val totalItemCount: Int = linearLayoutManager.getItemCount()
+//                    val firstVisiblesItems: Int =
+//                        linearLayoutManager.findFirstVisibleItemPosition()
+//
+//                    if (isLoading==false && (visibleItemCount + firstVisiblesItems) >= totalItemCount) {
+//                        doLoadMore()
+//
+//                    }
+//                }
+//            }
+//        }
+
         newslistRecyclerView.addOnScrollListener(scrollListener)
+
+    }
+
+    fun doLoadMore() {
+        isLoading = true;
+
+        var page_toload = page_param + 1
+
+        Log.e("load more", "page: ${page_param}")
+        Log.e("load more", "page toload: ${page_toload}")
+        Log.e("load more", "total page: ${totalpage}")
+
+        if (page_toload <= totalpage) {
+            page_param = page_toload
+            getData(query, page_toload)
+        }
 
     }
 
     private val viewModel: SearchViewModel by viewModels()
     private var items = ArrayList<Any>()
+    private var is_added = false
     private fun onGetSourcesObserver(_page: Int) = with(viewbinding) {
         viewModel.getEverything.observe(viewLifecycleOwner, Observer {
             when (it.status) {
                 Resource.Status.SUCCESS -> {
-                    Log.e("count article", "status: ${Resource.Status.SUCCESS}")
+                    isLoading = false
                     newslistSwipeContainer.isRefreshing = false
-                    if (it.data != null) {
+                    Log.e("count article", "it.data: ${it.data}")
+                    Log.e("count article", "is_added: ${is_added}")
+                    if (it.data != null && is_added == false) {
+                        is_added = true
+                        Log.e("count article", "status: ${Resource.Status.SUCCESS}")
                         total_articles = it.data.totalResults
                         totalpage = (total_articles / ConstantsObj.EVERYTHING_PAGE_SIZE).toFloat()
                             .roundToInt()
@@ -128,11 +158,13 @@ class SearchFragment : BaseFragmentBinding<FragmentNewslistPageBinding>(),
                     }
                 }
                 Resource.Status.ERROR -> {
+                    isLoading = false
                     newslistSwipeContainer.isRefreshing = false
                     Timber.e("getSearchEndpoint.observe: error")
                     Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
                 }
                 Resource.Status.LOADING -> {
+                    isLoading = false
                     Timber.e("getSearchEndpoint.observe: LOADING")
                     newslistSwipeContainer.isRefreshing = true
                 }
@@ -157,6 +189,7 @@ class SearchFragment : BaseFragmentBinding<FragmentNewslistPageBinding>(),
 
         items.clear()
 
+        page_param = 1
         getData(_query, 1)
     }
 
@@ -170,7 +203,8 @@ class SearchFragment : BaseFragmentBinding<FragmentNewslistPageBinding>(),
 
         Log.e("onGetSourcesObserver", "hashMap size= (${hashMap.size})")
         Log.e("onGetSourcesObserver", "onGetSourcesObserver(${_page})")
-        requireActivity().viewModelStore.clear()
+
+        is_added = false
         viewModel.start(hashMap)
         onGetSourcesObserver(_page)
     }
